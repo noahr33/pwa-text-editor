ee5aadafc709f92c85971a050c00d0b48f7647e7
noahr33 <128255532+noahr33@users.noreply.github.com>
2023-10-16 22:16:40 -0500

    initial files

---
+++ client/webpack.config.js
@@ -0,0 +1,65 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const WebpackPwaManifest = require('webpack-pwa-manifest');
+const path = require('path');
+const { InjectManifest } = require('workbox-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+
+
+// TODO: Add and configure workbox plugins for a service worker and manifest file.
+// TODO: Add CSS loaders and babel to webpack.
+
+module.exports = () => {
+  return {
+    mode: 'development',
+    entry: {
+      main: './src/js/index.js',
+      install: './src/js/install.js'
+    },
+    output: {
+      filename: 'main.bundle.js',
+      path: path.resolve(__dirname, 'dist')
+    },
+    plugins: [
+      new MiniCssExtractPlugin(),
+      new HtmlWebpackPlugin({
+        template: './src/index.html'
+      }),
+      new WebpackPwaManifest({
+        name: 'PWA Text Editor',
+        short_name: 'Text Editor',
+        description: 'A text editor app.',
+        background_color: 'black',
+        theme_color: 'black',
+        display: 'standalone'
+      }),
+      new InjectManifest({
+        swDest: './sw.js'
+      })
+    ],
+
+    module: {
+      rules: [
+        {
+          test: /\.css$/i,
+          use: [MiniCssExtractPlugin.loader, 'css-loader']
+        },
+        {
+          test: /\.(png|svg|jpg|jpeg|gif)$/i,
+          type: 'asset/resource',
+        },
+        {
+          test: /\.(?:js|mjs|cjs)$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: [
+                ['@babel/preset-env', { targets: "defaults" }]
+              ]
+            }
+          }
+        }
+      ],
+    },
+  };
+};
